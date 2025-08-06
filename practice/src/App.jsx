36a1f7eda17cc9840c2e6d77abea9ea0154fa954
Akhil Anwer M <akhilanwarm@gmail.com>
2025-08-06 18:51:17 +0530

    practice folder set up

---
+++ practice/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Tour from "./pages/Tour";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/card" element={<Tour />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
